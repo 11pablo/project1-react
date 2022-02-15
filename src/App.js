f55d4335834404eff64a35426407077c9faee8d1
Pawel Wieczorek <pawel1wiecz@gmail.com>
2022-02-15 22:44:42 +0100

    navigation bar

--- src/App.js
+++ src/App.js
@@ -1,17 +1,19 @@
 import Hero from './component/Hero/Hero';
 import SearchForm from './component/SearchForm/SearchForm';
 import List from './component/List/List';
-import Container from './component/Container/Container'
+import Container from './component/Container/Container';
+import NavBar from './component/NavBar/NavBar';
 
 const App = () => {
   return (
-    <div>
+    <main>
+      <NavBar/>
       <Container>
         <Hero />
         <SearchForm />
         <List />
       </Container>
-    </div>
+    </main>
   );
 };
 
