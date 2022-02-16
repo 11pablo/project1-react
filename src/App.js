a9ca94386e86398b42e2aecb8201eb9721b6afef
Pawel Wieczorek <pawel1wiecz@gmail.com>
2022-02-16 19:41:16 +0100

    information about incorrect address

--- src/App.js
+++ src/App.js
@@ -1,17 +1,23 @@
-import Hero from './component/Hero/Hero';
-import SearchForm from './component/SearchForm/SearchForm';
-import List from './component/List/List';
 import Container from './component/Container/Container';
 import NavBar from './component/NavBar/NavBar';
+import Favorite from './component/Favorite/Favorite';
+import About from './component/About/About';
+import Home from './component/Home/Home';
+import WrongAdress from './component/WrongAddress/WrongAddress';
+import { Routes, Route } from 'react-router-dom'; //routes - może renderować różne treści w zależnie od adresu, Route - ustala jaki komponent ma się znaleźć pod jakim adresem.
 
 const App = () => {
   return (
     <main>
       <NavBar/>
       <Container>
-        <Hero />
-        <SearchForm />
-        <List />
+        <Routes>
+          <Route path="/" element={<Home />} /> {/*pod linkiem / renderuj komponent Home */}
+          <Route path="/about" element={<About />} />
+          <Route path="/favorite" element={<Favorite />} />
+          <Route path="*" element={<WrongAdress />}
+          />
+        </Routes>
       </Container>
     </main>
   );
---
+++ src/component/WrongAddress/WrongAddress.js
@@ -0,0 +1,10 @@
+import styles from './WrongAddress.module.scss'
+
+const NotFound = () => {
+    return (
+        <div className={styles.container}>
+            <h1>404 not found</h1>
+        </div>
+    )
+}
+export default NotFound;
